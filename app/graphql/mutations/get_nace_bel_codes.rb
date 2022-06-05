518b2e18216abb2b5258618b7b8629f7077c0a81
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-06-06 00:36:31 +0200

    Get Nace-Bel codes: Handle Parent Code

--- app/graphql/mutations/get_nace_bel_codes.rb
+++ app/graphql/mutations/get_nace_bel_codes.rb
@@ -3,11 +3,16 @@ module Mutations
     graphql_name "GetNaceBelCodes"
 
     argument :level, Integer, required: true
+    argument :parent_code, String, required: true
 
     field :nace_bel_codes, [Types::NaceBelCode], null: false
 
     def resolve(args)
-      nace_bel_codes = NaceBelCode.where(level: args[:level]).all
+      if args[:parent_code].blank?
+        nace_bel_codes = NaceBelCode.where(level: args[:level]).all
+      else
+        nace_bel_codes = NaceBelCode.where(level: args[:level], parent_code: args[:parent_code]).all
+      end
 
       MutationResult.call(
         obj: { nace_bel_codes: nace_bel_codes },
